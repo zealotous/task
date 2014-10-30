9de00fc90b203f67dcee130ba652def875de01bc
Feoktistov Mihail <m.feoktistov@dev.kokoc.com>
2014-10-30 14:35:34 +0300

    modified files

--- settings.py
+++
@@ -1,7 +0,0 @@
-
-DATABASE_SETTINGS = {'host': 'localhost',
-                     'port': 3306,
-                     'db': 'task',
-                     'user': 'root',
-                     'passwd': 'cnj1ljkvfnby', }
-
